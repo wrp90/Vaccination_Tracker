278c6a54ed412fc69c8d8bf689e148c95d052cc2
Will Papp <wpapp90@gmail.com>
2021-04-10 10:50:50 -0500

    can now display homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -30,4 +30,6 @@ router.get('/login', (req, res) => {
     }
     res.render('login');
 });
+
+module.exports = router;
   
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-// const exphbs = require('express-handlebars');
-// const routes = require('./controllers');
-// const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -11,7 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
@@ -25,14 +25,14 @@ const sess = {
 
 app.use(session(sess));
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
