d7f3c68d9494d88004bf19a495f6efcab9c24aea
riraq <riraq@mail.usf.edu>
2021-04-10 11:18:34 -0500

    added route for database to display

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+// const patientRoutes = require('./patientRoutes');
+// const vaccineRoutes = require('./vaccineRoutes');
+
+// router.use('/patients', patientRoutes);
+// router.use('/vaccines', vaccineRoutes);
+
+module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-// const { Vaccine, Patient } = require('../models');
+const { Vaccine, Patient } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
@@ -23,13 +23,36 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/database', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const patientData = await Patient.findAll({
+      include: [
+        {
+          model: Vaccine
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const patients = patientData.map((patient) => patient.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('database', {
+      patients,
+      //   logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/login', (req, res) => {
-    if (req.session.logged_in) {
-      res.redirect('/');
-      return;
-    }
-    res.render('login');
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
 });
 
 module.exports = router;
-  
