993616cb23f84d23b7dbe31fdd26fc3dfb65f34c
riraq <riraq@mail.usf.edu>
2021-04-10 09:35:17 -0500

    updated seed file; added patient_id property to vaccine seed data

--- seeds/seed.js
+++ seeds/seed.js
@@ -7,17 +7,15 @@ const vaccineData = require('./vaccineData.json');
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const patients = await Patient.bulkCreate(patientData, {
+  await Patient.bulkCreate(patientData, {
     individualHooks: true,
     returning: true,
   });
 
-  for (const vaccine of vaccineData) {
-    await Vaccine.create({
-      ...vaccine,
-      patient_id: patients[Math.floor(Math.random() * patients.length)].id,
-    });
-  }
+  await Vaccine.bulkCreate(vaccineData, {
+    individualHooks: true,
+    returning: true,
+  });
 
   process.exit(0);
 };
