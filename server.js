9b9e51d4be5e2aaa02e9fcc2720894b8dae9b976
Stephen Solis <ssolis8619@gmail.com>
2021-04-13 17:27:00 -0500

    dashboard_updates

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
